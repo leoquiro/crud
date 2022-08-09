b381f848a8e9fc5472c868f7650417960db9e29a
Leonardo Rodrigues <odranoel.rodrigues@gmail.com>
2022-08-09 02:02:42 +0100

    git commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Start"
+        subtitle="Registration project">
+        <div className="display-4">Welcome!</div>
+        <hr />
+        <p className="mb-0">System to exemplify a Log in tool developed in react</p>
+    </Main>
